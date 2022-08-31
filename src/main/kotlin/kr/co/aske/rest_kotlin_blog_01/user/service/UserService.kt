4d7b35584fe54c4a6c8c882d2e08247c4a24c85a
TedyChang <teddychangstar@gmail.com>
2022-08-31 18:03:10 +0900

    feat(login): admin, admin 인 경우 로그인

--- src/main/kotlin/kr/co/aske/rest_kotlin_blog_01/user/service/UserService.kt
+++ src/main/kotlin/kr/co/aske/rest_kotlin_blog_01/user/service/UserService.kt
@@ -1,8 +1,11 @@
 package kr.co.aske.rest_kotlin_blog_01.user.service
 
 class UserService {
-    fun funcLogin(email:String, password: String): String {
-        return ""
+    fun funcLogin(email:String, password: String): Boolean {
+        if (email == "admin" && password == "admin"){
+            return true
+        }
+        return false
     }
 
 }
